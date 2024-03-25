f5a0e1243f4b3dc65b25888d30aa87c826640126
DerThorsten <derthorstenbeier@gmail.com>
2024-03-25 10:28:30 +0100

    xeus 4.0 updates

--- test/xmock_interpreter.hpp
+++ test/xmock_interpreter.hpp
@@ -21,16 +21,18 @@ namespace xeus
         xmock_interpreter() = default;
         virtual ~xmock_interpreter() = default;
 
+        
     private:
 
         void configure_impl() override;
 
-        nl::json execute_request_impl(int execution_counter,
+        void execute_request_impl(xrequest_context context,
+                                      send_reply_callback cb,
+                                      int execution_counter,
                                       const std::string& code,
-                                      bool silent,
-                                      bool store_history,
-                                      nl::json user_expressions,
-                                      bool allow_stdin) override;
+                                      execute_request_config config,
+                                      nl::json user_expressions
+                                      ) override;
 
         nl::json complete_request_impl(const std::string& code,
                                        int cursor_pos) override;
